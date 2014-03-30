4337fe727a185ea10460f44154d3fe72030f0169
ian asaff <ian.asaff@gmail.com>
2014-03-30 13:03:58 -0400

    added check_letter, test

--- lib/hangman.ex
+++ lib/hangman.ex
@@ -1,2 +1,28 @@
 defmodule Hangman do
+  def welcome do
+    IO.puts "Welcome"
+  end
+
+  def get_letter do
+    IO.getn "Pick a letter: \n"
+  end
+
+  def check_letter(letter, word, blanks) do
+    # flatten the returned list of list of tuples
+    # [[{idx,_}],[{idx,_}]]
+    indexes =
+      Regex.scan(~r/#{letter}/, word, return: :index)
+      |> List.flatten
+      |> Enum.map(&(elem(&1, 0)))
+
+    if Enum.any?(indexes) do
+      updated_blanks = Enum.reduce(indexes, blanks, fn(index, str) ->
+        List.replace_at(String.split(str, ""), index, letter)
+        |> Enum.join
+      end)
+      {:hit, updated_blanks}
+    else
+      {:miss, blanks}
+    end
+  end
 end
--- test/hangman_test.exs
+++ test/hangman_test.exs
@@ -1,7 +1,22 @@
 defmodule HangmanTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
 
-  test "the truth" do
-    assert(true)
+  # return from this callback must be
+  # {:ok, data} where data is a keyword list
+  # of stuff to reference in the tests
+  setup_all do
+    { :ok, word: "hello", blanks: "_e__o" }
+  end
+
+  #  |
+  #  0
+  # /|\
+  #_/ \_
+  test ".check_letter hit", meta do
+    assert Hangman.check_letter("l", meta[:word], meta[:blanks]) == {:hit, "_ello"}
+  end
+
+  test ".check_letter miss", meta do
+    assert Hangman.check_letter("x", meta[:word], meta[:blanks]) == {:miss, meta[:blanks]}
   end
 end
