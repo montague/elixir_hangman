d82590f2f28ec4cd0be1cd4b533b373fcec2ec25
ian asaff <ian.asaff@gmail.com>
2014-04-13 11:04:00 -0400

    basics working

--- lib/hangman.ex
+++ lib/hangman.ex
@@ -1,10 +1,11 @@
 defmodule Hangman do
+  @misses_allowed 8
 
   def run! do
     welcome
     word = "hello"
     blanks = blanks_for word
-    guess_a_letter(word, blanks)
+    guess_a_letter(word, blanks, 0, [])
   end
 
   def welcome do
@@ -18,26 +19,55 @@ defmodule Hangman do
     String.replace(word, ~r/./, "_")
   end
 
-  # get letter, check for win
-  # if no win, update board and print out, call again
-  # else, print out win, call "run"
-  #def game_loop({ :win }, word, blanks) do
-    #IO.puts "YOU WIN!\n"
-  #end
+  def format_blanks_for_print(blanks) do
+    String.split(blanks,"") |> Enum.join(" ") |> String.rstrip(? )
+  end
 
-  def guess_a_letter(word, blanks) do
+  def guess_a_letter(word, blanks, misses, letters_guessed) do
     IO.puts """
-    current status:
-    #{blanks}
+    Letters guessed: #{letters_guessed}
+    Misses left: #{@misses_allowed - misses}
+    #{get_status(misses)}
+
+
+
+    #{format_blanks_for_print(blanks)}
     """
     # poor man's chomp
-    guess = get_letter |> String.rstrip(?\n)
-    { _, blanks } = check_letter(guess, word, blanks)
-    if is_solved?(blanks) do
-      { :win }
-    else
-      guess_a_letter(word, blanks)
+    guess = get_letter |> String.first
+    letters_guessed = letters_guessed ++ [guess]
+    { result, blanks } = check_letter(guess, word, blanks)
+
+    cond do
+      is_solved?(blanks) ->
+        IO.puts """
+        #{format_blanks_for_print(blanks)}
+        You win!
+        """
+      result == :hit ->
+        guess_a_letter(word, blanks, misses, letters_guessed)
+      result == :miss ->
+        if misses + 1 == 8 do
+          IO.puts "GAME OVER"
+        else
+          guess_a_letter(word, blanks, misses + 1, letters_guessed)
+        end
+      true ->
+        IO.puts "WHY AM I HERE"
     end
+  end
+
+  def get_status(misses) do
+    status = "  | "
+    if misses > 0, do: status = status <> "\n  0"
+    if misses > 1, do: status = status <> "\n /"
+    if misses > 2, do: status = status <> "|"
+    if misses > 3, do: status = status <> "\\"
+    if misses > 4, do: status = status <> "\n_"
+    if misses > 5, do: status = status <> "/"
+    if misses > 6, do: status = status <> " \\"
+    if misses > 7, do: status = status <> "_"
+    status
   end
 
   def is_solved?(blanks) do
@@ -46,7 +76,7 @@ defmodule Hangman do
 
   def get_letter do
     # grab letter and newline char
-    IO.getn "Pick a letter: \n", 2
+    IO.gets "Pick a letter: "
   end
 
   def check_letter(letter, word, blanks) do
